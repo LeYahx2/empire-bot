9b4c63486d540c54f2431d1a5afe11b40b4f7c6f
LeYahx2 <yahya.anis2013@yahoo.com>
2026-05-09 15:14:54 +0200

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const {
+  Client,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  SlashCommandBuilder
+} = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -8,8 +14,38 @@ const client = new Client({
   ]
 });
 
-client.once('ready', () => {
-  console.log('L’Empire est en ligne 👑');
+const commands = [
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Répond pong')
+    .toJSON()
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+client.once('ready', async () => {
+  console.log('👑 L’Empire est en ligne');
+
+  try {
+    console.log('⏳ Synchronisation des slash commands...');
+
+    await rest.put(
+      Routes.applicationCommands('1500891961320542338'),
+      { body: commands }
+    );
+
+    console.log('✅ Slash commands synchronisées.');
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('🏓 pong');
+  }
 });
 
 client.on('messageCreate', message => {
